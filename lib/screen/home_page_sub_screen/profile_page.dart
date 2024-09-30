c1a6df9895b748dfa1150fa6c24b5117266b8da0
bsbacon0966 <jerry109a@gmail.com>
2024-09-30 20:23:31 +0800

    !adjust profile _buildField format

--- lib/screen/home_page_sub_screen/profile_page.dart
+++ lib/screen/home_page_sub_screen/profile_page.dart
@@ -6,71 +6,77 @@ class EditProfilePage extends StatefulWidget {
 }
 
 class _EditProfilePageState extends State<EditProfilePage> {
-  
+
   Map<String, String?> _profileData = {
     'bio': null,
     'name': null,
     'nickname': null,
     'phone': null,
+    'home':null,
     'gender': null,
     'school':null,
     'marital_status': null,
     'birth_date': null,
+    'career':null,
+    'eat_habit':null,
   };
 
   Map<String, List<String>> _dropdownOptions = {
     'gender': ['Male', 'Female', 'Non-Binary'],
     'marital_status': ['Single', 'Married'],
+    'zodiac_signs':['Aries','Taurus','Gemini','Cancer','Leo','Virgo','Libra','Scorpio','Sagittarius','Capricorn','Aquarius','Pisces'],
+    'eat_habit':['meat-based','Vegan','Lacto-vegetarian'],
   };
 
-  Widget _buildDropdown(String label, String key,{isRequired=false}) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 8.0),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          SizedBox(
-            width: 100,
-            child: RichText(
-              text:TextSpan(
-                text: label,
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                  children: [
-                  if (isRequired)
-                    TextSpan(
-                      text: ' *',
-                      style: TextStyle(color: Colors.red, fontSize: 16),
-                    ),
-                ],
-              )
+Widget _buildDropdown(String label, String key, {isRequired = false}) {
+  return Padding(
+    padding: const EdgeInsets.symmetric(vertical: 8.0),
+    child: Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        SizedBox(
+          width: 140,
+          child: RichText(
+            text: TextSpan(
+              text: label,
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              children: [
+                if (isRequired)
+                  TextSpan(
+                    text: ' *',
+                    style: TextStyle(color: Colors.red, fontSize: 16),
+                  ),
+              ],
             ),
           ),
-          Expanded(
-            child: DropdownButtonFormField<String>(
-              value: _profileData[key],
-              isExpanded: true,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                contentPadding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-              ),
-              hint: Text('請選擇'),
-              items: _dropdownOptions[key]?.map((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value),
-                );
-              }).toList() ?? [],
-              onChanged: (String? newValue) {
-                setState(() {
-                  _profileData[key] = newValue;
-                });
-              },
+        ),
+        Expanded(
+          child: DropdownButtonFormField<String>(
+            value: _profileData[key],
+            isExpanded: true,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              contentPadding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
             ),
+            hint: Text('請選擇'),
+            items: _dropdownOptions[key]?.map((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
+              );
+            }).toList() ?? [],
+            onChanged: (String? newValue) {
+              setState(() {
+                _profileData[key] = newValue;
+              });
+            },
           ),
-        ],
-      ),
-    );
-  }
+        ),
+      ],
+    ),
+  );
+}
+
 Widget _buildDatePicker(String label, String key) {
   return Padding(
     padding: const EdgeInsets.symmetric(vertical: 8.0),
@@ -78,7 +84,7 @@ Widget _buildDatePicker(String label, String key) {
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
         SizedBox(
-          width: 100,
+          width: 140,
           child: Text(
             label,
             style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
@@ -93,25 +99,74 @@ Widget _buildDatePicker(String label, String key) {
                 firstDate: DateTime(1900),
                 lastDate: DateTime.now(),
               );
-              
+
               if (selectedDate != null) {
                 setState(() {
                   _profileData[key] = "${selectedDate.year}/${selectedDate.month}/${selectedDate.day}";
                 });
               }
             },
-            child: IgnorePointer( // 使點擊的時候讀取先跳到上面DataPicker，這裡只做顯示用，Ignore此處TextField
+            child: IgnorePointer(
               child: TextFormField(
                 decoration: InputDecoration(
                   border: OutlineInputBorder(),
                   contentPadding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
                   hintText: _profileData[key] ?? '請選擇日期',
                   hintStyle: TextStyle(
-                    color: _profileData[key] == null ? Colors.grey : Colors.black, 
+                    color: _profileData[key] == null ? Colors.black54 : Colors.black,
                   ),
                 ),
               ),
             ),
+          ),
+        ),
+      ],
+    ),
+  );
+}
+
+  Widget _buildTextField(String label, String key, {int maxLines = 1, bool isRequired = false}) {
+  return Padding(
+    padding: const EdgeInsets.symmetric(vertical: 8.0),
+    child: Row(
+      crossAxisAlignment: CrossAxisAlignment.center, // 確保所有文本在垂直方向上對齊
+      children: [
+        SizedBox(
+          width: 140,
+          child: RichText(
+            text: TextSpan(
+              text: label,
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.black),
+              children: [
+                if (isRequired)
+                  TextSpan(
+                    text: ' *',
+                    style: TextStyle(color: Colors.red, fontSize: 16),
+                  ),
+              ],
+            ),
+          ),
+        ),
+        Expanded(
+          child: TextFormField(
+            maxLines: maxLines,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              contentPadding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+            ),
+            validator: isRequired
+                ? (value) {
+                    if (value == null || value.isEmpty) {
+                      return '請輸入$label';
+                    }
+                    return null;
+                  }
+                : null,
+            onChanged: (value) {
+              setState(() {
+                _profileData[key] = value;
+              });
+            },
           ),
         ),
       ],
@@ -153,62 +208,17 @@ Widget _buildDatePicker(String label, String key) {
           _buildTextField('暱稱', 'nickname',isRequired: true),
           _buildTextField('電話', 'phone', isRequired: true),
           _buildDropdown('性別', 'gender', isRequired:true),
+          _buildDatePicker('出生日期', 'birth_date'),
+          _buildDropdown('飲食習慣', 'eat_habit'),
+          _buildTextField('職業', 'career'),
           _buildTextField('學校', 'school'),
+          _buildTextField('居住地', 'home'),
           _buildDropdown('婚姻狀況', 'marital_status'),
-          _buildDatePicker('出生日期', 'birth_date'), 
+          _buildDropdown('星座', 'zodiac_signs'),
           SizedBox(height: 20),
           ElevatedButton(
             child: Text('保存'),
             onPressed: _saveProfile,
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildTextField(String label, String key, {int maxLines = 1, bool isRequired = false}) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 8.0),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          SizedBox(
-            width: 100,
-            child: RichText(
-              text: TextSpan(
-                text: label,
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.black),
-                children: [
-                  if (isRequired)
-                    TextSpan(
-                      text: ' *',
-                      style: TextStyle(color: Colors.red, fontSize: 16),
-                    ),
-                ],
-              ),
-            ),
-          ),
-          Expanded(
-            child: TextFormField(
-              maxLines: maxLines,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                contentPadding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-              ),
-              validator: isRequired
-                  ? (value) {
-                      if (value == null || value.isEmpty) {
-                        return '請輸入$label';
-                      }
-                      return null;
-                    }
-                  : null,
-              onChanged: (value) {
-                setState(() {
-                  _profileData[key] = value;
-                });
-              },
-            ),
           ),
         ],
       ),
