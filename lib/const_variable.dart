3209696b992319a1df9e6dd39b9b16edc2466b0d
bsbacon0966 <jerry109a@gmail.com>
2024-10-03 13:00:12 +0800

    !add Profile const List<String>for infomation

--- lib/const_variable.dart
+++ lib/const_variable.dart
@@ -1,7 +1,14 @@
-const List<String> genderList = ['Male', 'Female', 'Non-Binary', 'Secret'];
+//Using these infomation in Profile
+const List<String> genderList = ['Male', 'Female', 'Non-Binary', 'Prefer not to say'];
+const List<String> educationLevelList = ['No Formal Education','Primary School','Secondary School','Undergraduate','Postgraduate','PhD','Prefer not to say'];
+const List<String> mbtiList = ['INTJ', 'INTP', 'ENTJ', 'ENTP', 'INFJ', 'INFP', 'ENFJ', 'ENFP', 'ISTJ', 'ISFJ', 'ESTJ', 'ESFJ', 'ISTP', 'ISFP', 'ESTP', 'ESFP', 'Prefer not to say'];
+const List<String> zodiacSigns = ['Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn', 'Aquarius', 'Pisces', 'Prefer not to say'];
+const List<String> bloodTypeList = ['A','B','AB','O','Other','Prefer not to say'];
+const List<String> religionList = ['Agnostic', 'Atheist', 'Buddhist', 'Catholic', 'Christian', 'Hindu', 'Jewish', 'Muslim', 'Sikh', 'Spiritual', 'Orthodox Christian', 'Protestant', 'Shinto', 'Taoist', 'Other', 'Prefer not to say'];
+const List<String> sexualityList = ['Straight', 'Gay', 'Lesbian', 'Bisexual','Asexual', 'Pansexual', 'Queer', 'Questioning', 'Not listed', 'Prefer not to say'];
+const List<String> ethnicityList = ['Black/African Descent', 'East Asian', 'South Asian', 'Southeast Asian', 'Hispanic/Latino', 'Middle Eastern/North African', 'Native American/Indigenous', 'Pacific Islander','White/Caucasian', 'Multiracial', 'Other', 'Prefer not to say']
+const List<String> dietList = ['Omnivore', 'Vegetarian', 'Vegan', 'Pescatarian', 'Ketogenic', 'Paleo','Other', 'Prefer not to say'];
 const List<String> materialStatue = ['Single', 'Married', 'Secret'];
-const List<String> zodiacSigns = ['Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn', 'Aquarius', 'Pisces', 'Secret'];
-const List<String> eatHabit = ['meat-based', 'Vegan', 'Lacto-vegetarian'];
 const List<String> eventType = ['Music', 'Traval', 'Photography', 'Reading', 'Sports', 'Draw', 'Dancing','Fashion','Movie'];
 
 //Phone Number Checker 10 ~ 15 Digits
