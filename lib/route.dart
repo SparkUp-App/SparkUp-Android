b4506108e288f71c26f5aed206c0fb55f5be7422
bsbacon0966 <jerry109a@gmail.com>
2024-10-05 20:33:48 +0800

    !add initial profileData infomation page (two page , first page is for necessary infomation , second page is for not necessary data)

--- lib/route.dart
+++ lib/route.dart
@@ -2,15 +2,18 @@ import "package:flutter/material.dart";
 import "package:spark_up/screen/login_page.dart";
 import "package:spark_up/screen/register_page.dart";
 import "package:spark_up/screen/home_page.dart";
+import "package:spark_up/screen/initial_profile_create/initial_profileData_basicInfo.dart";
 class RouteMap{
   //Regist Path for page
   static const loginPage = "/loginPage";
   static const registerPage = "/registerPage";
   static const homePage = "/homePage";
+  static const initialProfileDataPage = "/initialProfileDataPage";
   //Bind page to Path
   static Map<String, WidgetBuilder> routes = {
     loginPage : (context) => const LoginPage(),
     registerPage : (context) => const RegisterPage(),
     homePage : (context) => const HomePage(),
+    initialProfileDataPage : (context) => const BasicProfilePage(),
   };
 }
