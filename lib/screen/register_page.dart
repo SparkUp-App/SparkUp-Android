d9929af39e1ad35225641a45c993b3aa676629e5
YanTing0Volleyball <yanting.wkact810611@gmail.com>
2024-10-12 02:09:29 +0800

    Add Back Arrow In Regist Page

--- lib/screen/register_page.dart
+++ lib/screen/register_page.dart
@@ -18,7 +18,9 @@ class _RegisterPageState extends State<RegisterPage> {
   var confirmPasswordController = TextEditingController();
   bool isLoading = false;
 
-  Widget login_TextField(IconData textFieldIcon, String label, String hintText, TextEditingController controller, {bool isObscured = false}) {
+  Widget login_TextField(IconData textFieldIcon, String label, String hintText,
+      TextEditingController controller,
+      {bool isObscured = false}) {
     return Padding(
       padding: EdgeInsets.fromLTRB(0.0, 7.0, 0.0, 0.0),
       child: Column(
@@ -43,8 +45,8 @@ class _RegisterPageState extends State<RegisterPage> {
               controller: controller,
               obscureText: isObscured,
               decoration: InputDecoration(
-                filled: true, 
-                fillColor: Colors.white, 
+                filled: true,
+                fillColor: Colors.white,
                 enabledBorder: OutlineInputBorder(
                   borderSide: const BorderSide(color: Color(0xFFE9765B)),
                   borderRadius: BorderRadius.circular(10.0),
@@ -85,97 +87,120 @@ class _RegisterPageState extends State<RegisterPage> {
       child: Stack(
         children: [
           Scaffold(
+            appBar: AppBar(
+              backgroundColor: Colors.transparent,
+              leading: IconButton(
+                icon: const Icon(Icons.arrow_back, size: 30.0,),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ),
             backgroundColor: Colors.transparent,
             body: Center(
               child: SingleChildScrollView(
-              child: Container(
-                margin: const EdgeInsets.symmetric(vertical: 50.0, horizontal: 25.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    SizedBox(height: 100,),//確定他是否可以scroll
-                    login_TextField(Icons.email, 'Email', 'Email Address', emailController),
-                    login_TextField(Icons.lock, 'Password', 'Password', passwordController, isObscured: true),
-                    login_TextField(Icons.lock, 'Confirm Password', 'Confirm Password', confirmPasswordController, isObscured: true),
-                    SizedBox(height: 70),
-                    Center(
-                      child: SizedBox(
-                        width: 220,
-                        height: 47,
-                        child: ElevatedButton(
-                          onPressed: () async {
-                            if (passwordController.text != confirmPasswordController.text) {
-                              showDialog( //這裡我先多check密碼與確認密碼是否匹配
-                                context: context,
-                                builder: (context) => AlertDialog(
-                                  title: Text('Error'),
-                                  content: Text('Passwords do not match!'),
-                                  actions: [
-                                    TextButton(
-                                      onPressed: () {
-                                        Navigator.of(context).pop();
-                                      },
-                                      child: Text('OK'),
-                                    ),
-                                  ],
-                                ),
-                              );
-                              return;
-                            }
-                            setState(() {
-                              isLoading = true;
-                            });
-
-                            debugPrint("Sending Register Request");
-                            final response = await Network.manager.sendRequest(
-                              method: RequestMethod.post,
-                              path: AuthPath.register,
-                              data: {
-                                "email": emailController.text,
-                                "password": passwordController.text,
-                              },
-                            );
-                            debugPrint("Register Request Finished");
-
-                            setState(() {
-                              isLoading = false;
-                            });
-
-                            if (context.mounted) {
-                              if (response["status"] == "success") {
-                                Network.manager.saveUserId(response["data"]["user_id"]);
-                                Profile.manager = Profile.initfromDefault();
-                                Navigator.pushReplacementNamed(context, RouteMap.initialProfileDataPage);
-                              } else {
+                child: Container(
+                  margin: const EdgeInsets.symmetric(
+                      vertical: 50.0, horizontal: 25.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                        height: 100,
+                      ), //確定他是否可以scroll
+                      login_TextField(Icons.email, 'Email', 'Email Address',
+                          emailController),
+                      login_TextField(Icons.lock, 'Password', 'Password',
+                          passwordController,
+                          isObscured: true),
+                      login_TextField(Icons.lock, 'Confirm Password',
+                          'Confirm Password', confirmPasswordController,
+                          isObscured: true),
+                      SizedBox(height: 70),
+                      Center(
+                        child: SizedBox(
+                          width: 220,
+                          height: 47,
+                          child: ElevatedButton(
+                            onPressed: () async {
+                              if (passwordController.text !=
+                                  confirmPasswordController.text) {
                                 showDialog(
+                                  //這裡我先多check密碼與確認密碼是否匹配
                                   context: context,
-                                  builder: (context) => SystemMessage(content: response["data"]["message"]),
+                                  builder: (context) => AlertDialog(
+                                    title: Text('Error'),
+                                    content: Text('Passwords do not match!'),
+                                    actions: [
+                                      TextButton(
+                                        onPressed: () {
+                                          Navigator.of(context).pop();
+                                        },
+                                        child: Text('OK'),
+                                      ),
+                                    ],
+                                  ),
                                 );
+                                return;
                               }
-                            }
-                          },
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: Color(0xFFF16743),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(20),
+                              setState(() {
+                                isLoading = true;
+                              });
+
+                              debugPrint("Sending Register Request");
+                              final response =
+                                  await Network.manager.sendRequest(
+                                method: RequestMethod.post,
+                                path: AuthPath.register,
+                                data: {
+                                  "email": emailController.text,
+                                  "password": passwordController.text,
+                                },
+                              );
+                              debugPrint("Register Request Finished");
+
+                              setState(() {
+                                isLoading = false;
+                              });
+
+                              if (context.mounted) {
+                                if (response["status"] == "success") {
+                                  Network.manager
+                                      .saveUserId(response["data"]["user_id"]);
+                                  Profile.manager = Profile.initfromDefault();
+                                  Navigator.pushReplacementNamed(
+                                      context, RouteMap.initialProfileDataPage);
+                                } else {
+                                  showDialog(
+                                    context: context,
+                                    builder: (context) => SystemMessage(
+                                        content: response["data"]["message"]),
+                                  );
+                                }
+                              }
+                            },
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Color(0xFFF16743),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20),
+                              ),
                             ),
-                          ),
-                          child: const Text(
-                            'Register!',
-                            style: TextStyle(
-                              fontFamily: 'IowanOldStyle',
-                              color: Colors.white,
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
+                            child: const Text(
+                              'Register!',
+                              style: TextStyle(
+                                fontFamily: 'IowanOldStyle',
+                                color: Colors.white,
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
                             ),
                           ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
               ),
             ),
           ),
