bf3a650943dae74e34b1a26602947faf497450b5
YanTing0Volleyball <yanting.wkact810611@gmail.com>
2024-10-13 16:01:50 +0800

    Buile Template Event Detail Page And Regist In Route Map

---
+++ lib/screen/home_page_sub_screen/event_detail_page.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class EventDetailPage extends StatefulWidget {
+  const EventDetailPage({super.key, required this.postId});
+
+  final int postId;
+
+  @override
+  State<EventDetailPage> createState() => _EventDetailPageState();
+}
+
+class _EventDetailPageState extends State<EventDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () => Navigator.pop(context),
+          icon: const Icon(
+            Icons.arrow_back,
+            size: 30.0,
+          ),
+        ),
+      ),
+      body: const Center(
+        child: Text("Page is in programming..."),
+      ),
+    );
+  }
+}
