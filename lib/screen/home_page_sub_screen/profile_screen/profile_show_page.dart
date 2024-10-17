1c6d410b72792cb1ba1e57086e93ef070f36c8ef
bsbacon0966 <jerry109a@gmail.com>
2024-10-17 17:35:33 +0800

    !adjust profile_show_page ui design ， replace lots of construct to make it more like "ins"

--- lib/screen/home_page_sub_screen/profile_screen/profile_show_page.dart
+++ lib/screen/home_page_sub_screen/profile_screen/profile_show_page.dart
@@ -13,159 +13,281 @@ class ProfileShowPage extends StatefulWidget {
 class _ProfileShowPageState extends State<ProfileShowPage> with TickerProviderStateMixin {
   late TabController _tabController;
 
-  Widget _buildStatColumn(String label, String count) {
+  Widget _buildStatColumn(String label, String count,IconData stateIcon) {
     return Column(
       mainAxisSize: MainAxisSize.min,
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         Text(
           label,
-          style: TextStyle(color: Colors.white),
-        ),
-        Text(
-          count,
           style: TextStyle(
-            fontSize: 24, 
-            fontWeight: FontWeight.bold,
             color: Colors.white,
+            fontWeight: FontWeight.w600,
           ),
         ),
+        Row(
+          children: [
+            Icon(
+              stateIcon,
+              color: Colors.white,
+              size: 40,
+            ),
+            Text(
+              count,
+              style: TextStyle(
+                fontSize: 26,
+                fontWeight: FontWeight.w600,
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+
       ],
+    );
+  }
+
+  Widget profileHeaderWidget(BuildContext context) { //header 設計直接複製貼上
+    return Container(
+      decoration: BoxDecoration(
+        color: Color(0xFFF7AF8B),
+      ),
+      height: 250,
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+            flex: 2,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(8),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(height: 10),
+                      Container(
+                        height: 70,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            _buildStatColumn('Participated', '30',Icons.flag),
+                            SizedBox(width: 16),
+                            _buildStatColumn('Rating', '4.8',Icons.star),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        height: 95,
+                        width: MediaQuery.of(context).size.width * 0.75,
+                        child: Padding(
+                          padding: EdgeInsets.all(8),
+                          child: Text(
+                            Profile.manager.bio,
+                            style: TextStyle(
+                                color: Colors.white
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 8.0),
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Expanded(
+                              child: Container(
+                                height: 40, // 調整整體高度以縮小
+                                child: Row(
+                                  children: [
+                                    Expanded(
+                                      child: ListView.builder(
+                                        scrollDirection: Axis.horizontal,
+                                        itemCount: Profile.manager.interestTypes.length,
+                                        itemBuilder: (context, index) {
+                                          final tag = Profile.manager.interestTypes[index];
+                                          return Padding(
+                                            padding: const EdgeInsets.symmetric(horizontal: 2), // 減少間距
+                                            child: Chip(
+                                              label: Text(
+                                                '#$tag',
+                                                style: TextStyle(
+                                                  fontSize: 10, // 調整標籤字體大小以縮小
+                                                ),
+                                              ),
+                                              backgroundColor: Colors.grey[200],
+                                              padding: EdgeInsets.symmetric(horizontal: 2), // 調整 Chip 的內邊距
+                                              shape: RoundedRectangleBorder(
+                                                borderRadius: BorderRadius.circular(20),// 調整圓角半徑
+                                              ),
+                                            ),
+                                          );
+                                        },
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Column(
+              children: [
+                SizedBox(height: 20),
+                Center(
+                  child: CircleAvatar(
+                    radius: 50,
+                  ),
+                ),
+                SizedBox(height: 16),
+                Container(
+                  width: 130,
+                  height: 25,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xFFF16743),
+                    ),
+                    onPressed: () => Navigator.pushNamed(context, RouteMap.editProfile),
+                    child: Text(
+                      "Edit Profile",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 16),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 
   @override
   void initState() {
     super.initState();
-    _tabController = TabController(length: 2, vsync: this);
+    _tabController = TabController(length: 3, vsync: this);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(Profile.manager.nickname),
-        backgroundColor: Colors.white,
-        foregroundColor: Colors.black,
-        elevation: 0,
-      ),
-      body: SingleChildScrollView( //他"整體"是要能夠滑動的，所以加一個singleScroll
-        child: Column(
-          children: [
-            Container( //個人資訊頁面是有背景顏色的，所以將其包住
-              decoration: BoxDecoration(
-                color: Color(0xFFF7AF8B),
-              ),
-              height: 250,//固定高度為250
-              child: Row( 
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Expanded( //設定他占整個row 2份比例(2:1)，包含參加過的活動數與評價，以及個人介紹(Bio，現階段還未連結Profile.manager)
-                    flex: 2,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Padding(
-                          padding: EdgeInsets.all(8),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              SizedBox(height: 20),
-                              Container(
-                                height: 60,
-                                child:  Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                                children: [
-                                  _buildStatColumn('Participated', '30'),
-                                  SizedBox(width: 16),
-                                  _buildStatColumn('Rating', '4.8'),
-                                ],
-                              ),
-                              ),
-                              
-                              Container(
-                                height: 125,
-                                width: MediaQuery.of(context).size.width * 0.75,
-                                child: Padding(
-                                  padding: EdgeInsets.all(8),
-                                  child: Text(
-                                    Profile.manager.bio,
-                                    style: TextStyle(
-                                      color: Colors.white
-                                    ),
-                                  ),
-                                ),
-                              ),
-                              
-                            ],
-                          ),
-                        ),
-                      ],
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(57.0), // 調整AppBar的高度
+        child: Container(
+          color: Color(0xFFF7AF8B), // 與Container相同的顏色
+          child: Column(
+            children: [
+              AppBar(
+                automaticallyImplyLeading: false,
+                title: Text(
+                  Profile.manager.nickname,
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.white,
+                  ), // 縮小字體大小
+                ),
+                backgroundColor: Colors.transparent, // 透明背景
+                foregroundColor: Colors.black,
+                elevation: 0, // 去掉陰影
+                actions: [
+                  IconButton(
+                    icon: Icon(
+                      Icons.settings,
+                      color: Colors.white,
                     ),
-                  ),
-                  Expanded(//這裡放一個"假頭貼"跟一個edit按鈕
-                    flex: 1,
-                    child: Column(
-                      children: [
-                        SizedBox(height: 20),
-                        Center(
-                          child: CircleAvatar(
-                            radius: 50,
-                          ),
-                        ),
-                        SizedBox(height: 16),
-                        Container(
-                          width: 130,
-                          height: 30,
-                          child: ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: const Color(0xFFF16743),
-                            ),
-                            onPressed: () => Navigator.pushNamed(context, RouteMap.editProfile),
-                            child: Text(
-                              "Edit Profile",
-                              style: TextStyle(
-                                color: Colors.white,
-                              ),
-                            ),
+                    onPressed: () {
+                      Navigator.pushNamed(context, RouteMap.editProfile);
+                    },
 
-                          ),
-                        ),
-                        SizedBox(height: 16),
-                      ],
-                    ),
                   ),
                 ],
               ),
-            ),
-            TabBar(
-              controller: _tabController,
-              tabs: [
-                Tab(icon: Icon(Icons.grid_on, color: Colors.black)),
-                Tab(icon: Icon(Icons.list, color: Colors.black)),
-              ],
-              indicatorColor: Colors.black,
-              unselectedLabelColor: Colors.grey,
-            ),
-            SizedBox(
-              height: 500, // 给定一个固定高度，或者根据需要调整
-              child: TabBarView(
-                controller: _tabController,
-                children: [
-                  _buildTabContent('Grid View Content'),
-                  _buildTabContent('List View Content'),
-                ],
+              Container(
+                height: 1, // 白線的高度
+                color: Colors.white, // 白色
               ),
-            ),
-          ],
+            ],
+          ),
+        ),
+      ),
+      body: DefaultTabController(
+        length: 2,
+        child: NestedScrollView(
+          headerSliverBuilder: (context, _) { //設定header
+            return [
+              SliverList(
+                delegate: SliverChildListDelegate(
+                  [
+                    profileHeaderWidget(context),
+                  ],
+                ),
+              ),
+            ];
+          },
+          body: Column(
+            children: <Widget>[
+              Material(
+                color: Colors.white,
+                child: TabBar(
+                  controller: _tabController,
+                  labelColor: Colors.black,
+                  unselectedLabelColor: Colors.grey[400],
+                  indicatorWeight: 1,
+                  indicatorColor: Colors.black,
+                  tabs: [
+                    Tab(
+                      icon: Icon(
+                        Icons.grid_on_sharp,
+                        color: Colors.black,
+                      ),
+                    ),
+                    Tab(
+                      icon: Icon(
+                        Icons.grid_on_sharp,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: TabBarView(
+                  controller: _tabController,
+                  children: [
+                    _buildTabContent('Grid'),
+                    _buildTabContent('IGTV'),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 
   Widget _buildTabContent(String text) {
-    return Center(
-      child: Text(text, style: TextStyle(fontSize: 24)),
+    return ListView.builder(
+      itemCount: 20,
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Text('$text Item $index'),
+        );
+      },
     );
   }
 }
