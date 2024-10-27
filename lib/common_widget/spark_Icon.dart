0e18c18a25d5db5810f98d52738d72831f1fe69b
YanTing0Volleyball <yanting.wkact810611@gmail.com>
2024-10-27 20:22:53 +0800

    Create SparkIcon Widget

---
+++ lib/common_widget/spark_Icon.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class SparkIcon extends StatelessWidget {
+  final SparkIcons icon;
+  final double size;
+  final Color? color;
+  
+  const SparkIcon({
+    super.key,
+    required this.icon,
+    this.size = 24,
+    this.color,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SvgPicture.asset(
+      icon.path,
+      width: size,
+      height: size,
+      colorFilter: color != null 
+          ? ColorFilter.mode(color!, BlendMode.srcIn)
+          : null,
+    );
+  }
+}
+
+enum SparkIcons {
+  back('assets/icons/back.svg'),
+  comment('assets/icons/comment.svg'),
+  heart('assets/icons/heart.svg'),
+  alcohol('assets/icons/alcohol.svg'),
+  blood('assets/icons/blood.svg'),
+  bookmark('assets/icons/bookmark.svg'),
+  calendar('assets/icons/calendar.svg'),
+  car('assets/icons/car.svg'),
+  circleUser('assets/icons/circle_user.svg'),
+  city('assets/icons/city.svg'),
+  eat('assets/icons/eat.svg'),
+  education('assets/icons/education.svg'),
+  email('assets/icons/email.svg'),
+  flag('assets/icons/flag.svg'),
+  gender('assets/icons/gender.svg'),
+  globe('assets/icons/globe.svg'),
+  home('assets/icons/home.svg'),
+  hometown('assets/icons/hometown.svg'),
+  language('assets/icons/language.svg'),
+  location('assets/icons/location.svg'),
+  message('assets/icons/message.svg'),
+  password('assets/icons/password.svg'),
+  peace('assets/icons/peace.svg'),
+  person('assets/icons/person.svg'),
+  phone('assets/icons/phone.svg'),
+  plus('assets/icons/plus.svg'),
+  religion('assets/icons/religion.svg'),
+  school('assets/icons/school.svg'),
+  setting('assets/icons/setting.svg'),
+  sexuality('assets/icons/sexuality.svg'),
+  skill('assets/icons/skill.svg'),
+  smile('assets/icons/smile.svg'),
+  smoking('assets/icons/smoking.svg'),
+  star('assets/icons/star.svg'),
+  user('assets/icons/user.svg'),
+  users('assets/icons/users.svg'),
+  work('assets/icons/work.svg'),
+  zodiacSign('assets/icons/zodiac_sign.svg');
+
+  final String path;
+  const SparkIcons(this.path);
+
+  String get assetPath => path;
+}
