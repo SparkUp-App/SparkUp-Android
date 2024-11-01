da4e8cf7eabbd2a7b1d7491660e5007ff92303c7
YanTing0Volleyball <yanting.wkact810611@gmail.com>
2024-11-01 20:49:30 +0800

    Create Secure Storage

---
+++ lib/sercure_storage.dart
@@ -0,0 +1,26 @@
+
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class SecureStorage {
+  static const storage = FlutterSecureStorage();
+
+  static store(StoreKey key, String value) async {
+    storage.write(key: key.key, value: value);
+  }
+
+  static read(StoreKey key) async {
+    storage.read(key: key.key);
+  }
+
+  static delete(StoreKey key) async{
+    storage.delete(key: key.key);
+  }
+}
+
+enum StoreKey{
+  userId(key: "USER_ID"),
+  noProfile(key: "NO_PROFILE");
+
+  final String key;
+  const StoreKey({required this.key});
+}
