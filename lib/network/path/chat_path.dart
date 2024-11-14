1526aad1b15af42b8d30ec03987bbfa63f3299c9
YanTing0Volleyball <yanting.wkact810611@gmail.com>
2024-11-14 18:50:32 +0800

    Add Chat Path

---
+++ lib/network/path/chat_path.dart
@@ -0,0 +1,14 @@
+import 'package:spark_up/network/httpprotocol.dart';
+
+enum ChatPath implements HttpPath {
+  message(path: "chat/messages"),
+  rooms(path: "chat/rooms/%0");
+
+  @override
+  String get getPath => path;
+
+  final String path;
+  const ChatPath({
+    required this.path,
+  });
+}
