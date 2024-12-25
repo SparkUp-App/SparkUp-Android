4d0f1aac8a7ff34a85e7b7c784e671903510ff30
YanTing0Volleyball <yanting.wkact810611@gmail.com>
2024-12-25 21:43:00 +0800

    Fixed Event Comment Display Problem

--- lib/data/comment.dart
+++ lib/data/comment.dart
@@ -10,32 +10,38 @@ class Comment {
   late int likes;
   late String userNickName;
   late bool liked;
+  late int level;
 
-  Comment(
-      {required this.commentsId,
-      required this.postId,
-      required this.userId,
-      required this.content,
-      required this.deleted,
-      required this.createdDate,
-      required this.lastUpdateDate,
-      required this.floor,
-      required this.likes,
-      required this.userNickName,
-      required this.liked});
+  Comment({
+    required this.commentsId,
+    required this.postId,
+    required this.userId,
+    required this.content,
+    required this.deleted,
+    required this.createdDate,
+    required this.lastUpdateDate,
+    required this.floor,
+    required this.likes,
+    required this.userNickName,
+    required this.liked,
+    required this.level,
+  });
 
   factory Comment.initfromData(Map data) {
     return Comment(
-        commentsId: data["id"],
-        postId: data["post_id"],
-        userId: data["user_id"],
-        content: data["content"],
-        deleted: data["deleted"],
-        createdDate: DateTime.parse(data["comment_created_date"]).toLocal(),
-        lastUpdateDate: DateTime.parse(data["comment_last_updated_date"]).toLocal(),
-        floor: data["floor"],
-        likes: data["likes"],
-        userNickName: data["nickname"],
-        liked: data["liked"]);
+      commentsId: data["id"],
+      postId: data["post_id"],
+      userId: data["user_id"],
+      content: data["content"],
+      deleted: data["deleted"],
+      createdDate: DateTime.parse(data["comment_created_date"]).toLocal(),
+      lastUpdateDate:
+          DateTime.parse(data["comment_last_updated_date"]).toLocal(),
+      floor: data["floor"],
+      likes: data["likes"],
+      userNickName: data["nickname"],
+      liked: data["liked"],
+      level: data["level"],
+    );
   }
 }
