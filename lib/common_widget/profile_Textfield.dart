9a742c16ae9270cc7fdd756efbb7186c516c8ef3
bsbacon0966 <jerry109a@gmail.com>
2024-12-31 10:00:43 +0800

    !fix bug

--- lib/common_widget/profile_Textfield.dart
+++ lib/common_widget/profile_Textfield.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-
 class profileTextfield extends StatefulWidget {
-  const profileTextfield({
+  profileTextfield({ //要求:key，標籤，提示文字，此文字框的icon，他所對應的值，回調函數，是否為必填(預設false)，要開幾格空間給他(預設1)
     super.key,
     required this.label,
     required this.hintLabel,
@@ -11,15 +10,28 @@ class profileTextfield extends StatefulWidget {
     required this.onChanged,
     this.isRequired = false,
     this.maxLine = 1,
+    this.focusNode,
+    this.onSubmitted,
+    this.onTapOutside,
+    this.hint = false,
+    this.hintTextWidget,
+    this.keyboardType,
   });
 
   final String label;
   final String hintLabel;
   final String value;
   final String textFieldIcon;
-  final Function(String?) onChanged; // 回調函數
+  final Function(String?) onChanged; // 回條函數
   final bool isRequired;
   final int maxLine;
+  late FocusNode? focusNode;
+  final Function(String)? onSubmitted;
+  final Function(PointerDownEvent)? onTapOutside;
+  bool hint;
+  Widget? hintTextWidget;
+  TextInputType? keyboardType;
+
 
   @override
   State<profileTextfield> createState() => _profile_TextfieldState();
@@ -31,6 +43,8 @@ class _profile_TextfieldState extends State<profileTextfield> {
   @override
   void initState() {
     super.initState();
+
+    widget.focusNode = widget.focusNode ?? FocusNode();
     textController = TextEditingController(text: widget.value);
   }
 
@@ -43,19 +57,22 @@ class _profile_TextfieldState extends State<profileTextfield> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
-              widget.label + (widget.isRequired ? " *" : ""), //Require 要多顯示 * 號
+              widget.label + (widget.isRequired ? " *" : ""),//Require 要多顯示 * 號
               style: const TextStyle(
                 fontSize: 16,
                 color: Color(0xFFE9765B),
                 fontWeight: FontWeight.w600,
               ),
             ),
+            if(widget.hint) widget.hintTextWidget ?? Container(),
             Container(
               padding: const EdgeInsets.symmetric(vertical: 2.0),
               width: MediaQuery.of(context).size.width * 0.75,
               child: TextField(
+                keyboardType: widget.keyboardType ?? TextInputType.text,
                 maxLines: widget.maxLine,
-                controller: textController,
+                focusNode: widget.focusNode,
+                controller:  textController,
                 decoration: InputDecoration(
                   filled: true,
                   fillColor: Colors.white,
@@ -72,23 +89,31 @@ class _profile_TextfieldState extends State<profileTextfield> {
                     borderRadius: BorderRadius.circular(10.0),
                   ),
                   prefixIcon: Padding(
-                    padding: const EdgeInsets.all(10.0), // 為 prefixIcon 添加間距
+                    padding: EdgeInsets.fromLTRB(16, 16, 16, 16),
                     child: SvgPicture.asset(
                       widget.textFieldIcon,
-                      fit: BoxFit.contain,
+                      width: 20,
+                      height: 20,
+                      colorFilter: ColorFilter.mode(
+                        Colors.black26,
+                        BlendMode.srcIn,
+                      ),
                     ),
                   ),
-                  prefixIconConstraints: const BoxConstraints(
-                    minWidth: 40, // 確保圖標寬度
-                    minHeight: 40, // 確保圖標高度
-                  ),
+
                   hintText: widget.hintLabel,
                   hintStyle: const TextStyle(
                     color: Colors.black26,
                   ),
                 ),
                 onChanged: (value) {
-                  widget.onChanged(value); // 將變更的值傳遞給父級
+                  widget.onChanged(value); // 将改变的值传递给父级
+                },
+                onTapOutside: widget.onTapOutside ?? (event){
+                  widget.focusNode?.unfocus();
+                },
+                onSubmitted: widget.onSubmitted ?? (value){
+                  widget.focusNode?.unfocus();
                 },
               ),
             ),
