d0421d1d63b2f1c25b18a643834c8815bd960fd5
YanTing0Volleyball <yanting.wkact810611@gmail.com>
2025-01-02 11:10:40 +0800

    Add Tool Tip Hint For Background Notification In Notification Setting Page

--- lib/screen/logout_page_sub_screen/notification_setting_page.dart
+++ lib/screen/logout_page_sub_screen/notification_setting_page.dart
@@ -6,7 +6,8 @@ class NotificationSettingPage extends StatefulWidget {
   const NotificationSettingPage({super.key});
 
   @override
-  State<NotificationSettingPage> createState() => _NotificationSettingPageState();
+  State<NotificationSettingPage> createState() =>
+      _NotificationSettingPageState();
 }
 
 class _NotificationSettingPageState extends State<NotificationSettingPage> {
@@ -44,42 +45,94 @@ class _NotificationSettingPageState extends State<NotificationSettingPage> {
               width: MediaQuery.of(context).size.width * 0.9,
               height: 20,
             ),
-            notificationSwitchWidget("Allow Notification", NotificationManager.allowNotification, StoreKey.allowNotification),
-            Container(
-              width: MediaQuery.of(context).size.width * 0.9,
-              alignment: Alignment.centerLeft,
-              margin: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 2.0),
-              padding: const EdgeInsets.only(left: 10.0),
-              child: const Text("Foreground Notification", style: TextStyle(color: Color(0xFF827C79), fontSize: 12.0)),
-            ),
-            notificationSwitchWidget("Chatroom message", NotificationManager.foregroundNewMessage, StoreKey.foregroundNewMessage),
-            notificationSwitchWidget("Approve message", NotificationManager.foregroundApproveMessage, StoreKey.foregroundApproveMessage),
-            notificationSwitchWidget("Reject message", NotificationManager.foregroundRejectMessage, StoreKey.foregroundRejectMessage),
-            notificationSwitchWidget("Apply message", NotificationManager.foregroundApplyMessage, StoreKey.foregroundApplyMessage),
-
+            notificationSwitchWidget(
+                "Allow Notification",
+                NotificationManager.allowNotification,
+                StoreKey.allowNotification),
             SizedBox(
               width: MediaQuery.of(context).size.width * 0.9,
               height: 20,
             ),
-            
             Container(
               width: MediaQuery.of(context).size.width * 0.9,
               alignment: Alignment.centerLeft,
-              margin: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 2.0),
+              margin:
+                  const EdgeInsets.symmetric(horizontal: 10.0, vertical: 2.0),
               padding: const EdgeInsets.only(left: 10.0),
-              child: const Text("Background Notification", style: TextStyle(color: Color(0xFF827C79), fontSize: 12.0)),
+              child: const Text("Foreground Notification",
+                  style: TextStyle(color: Color(0xFF827C79), fontSize: 12.0)),
             ),
-            notificationSwitchWidget("Chatroom message", NotificationManager.backgroundNewMessage, StoreKey.backgroundNewMessage),
-            notificationSwitchWidget("Approve message", NotificationManager.backgroundApproveMessage, StoreKey.backgroundApproveMessage),
-            notificationSwitchWidget("Reject message", NotificationManager.backgroundRejectMessage, StoreKey.backgroundRejectMessage),
-            notificationSwitchWidget("Apply message", NotificationManager.backgroundApplyMessage, StoreKey.backgroundApplyMessage),
+            notificationSwitchWidget(
+                "Chatroom message",
+                NotificationManager.foregroundNewMessage,
+                StoreKey.foregroundNewMessage),
+            notificationSwitchWidget(
+                "Approve message",
+                NotificationManager.foregroundApproveMessage,
+                StoreKey.foregroundApproveMessage),
+            notificationSwitchWidget(
+                "Reject message",
+                NotificationManager.foregroundRejectMessage,
+                StoreKey.foregroundRejectMessage),
+            notificationSwitchWidget(
+                "Apply message",
+                NotificationManager.foregroundApplyMessage,
+                StoreKey.foregroundApplyMessage),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.9,
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Container(
+                  alignment: Alignment.centerLeft,
+                  margin: const EdgeInsets.symmetric(
+                      horizontal: 10.0, vertical: 2.0),
+                  padding: const EdgeInsets.only(left: 10.0),
+                  child: const Text("Background Notification",
+                      style:
+                          TextStyle(color: Color(0xFF827C79), fontSize: 12.0)),
+                ),
+                const Tooltip(
+                  showDuration: Duration(seconds: 5),
+                  exitDuration: Duration(milliseconds: 500),
+                  triggerMode: TooltipTriggerMode.tap,
+                  margin: EdgeInsets.symmetric(horizontal: 8.0),
+                  message:
+                      "Background notification only work while app is running in the background. Before use this feature, please check the device permission first.",
+                  child: Icon(
+                    Icons.help_outline_rounded,
+                    color: Color(0xFF827C79),
+                    size: 18.0,
+                  ),
+                )
+              ],
+            ),
+            notificationSwitchWidget(
+                "Chatroom message",
+                NotificationManager.backgroundNewMessage,
+                StoreKey.backgroundNewMessage),
+            notificationSwitchWidget(
+                "Approve message",
+                NotificationManager.backgroundApproveMessage,
+                StoreKey.backgroundApproveMessage),
+            notificationSwitchWidget(
+                "Reject message",
+                NotificationManager.backgroundRejectMessage,
+                StoreKey.backgroundRejectMessage),
+            notificationSwitchWidget(
+                "Apply message",
+                NotificationManager.backgroundApplyMessage,
+                StoreKey.backgroundApplyMessage),
           ],
         ),
       ),
     );
   }
 
-  Widget notificationSwitchWidget(String label, bool notificationOn, StoreKey key){
+  Widget notificationSwitchWidget(
+      String label, bool notificationOn, StoreKey key) {
     return Container(
       margin: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
       padding: const EdgeInsets.all(10.0),
