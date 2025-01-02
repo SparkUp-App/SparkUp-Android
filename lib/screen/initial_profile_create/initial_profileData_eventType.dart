13fdedcbfaf028ec1913e1b077f389bfcc1cae62
YanTing0Volleyball <yanting.wkact810611@gmail.com>
2025-01-02 12:05:11 +0800

    Remove Wrong Import Cause Crash

--- lib/screen/initial_profile_create/initial_profileData_eventType.dart
+++ lib/screen/initial_profile_create/initial_profileData_eventType.dart
@@ -1,5 +1,3 @@
-import 'dart:js_interop';
-
 import 'package:flutter/material.dart';
 import 'package:spark_up/chat/chat_room_manager.dart';
 import 'package:spark_up/common_widget/system_message.dart';
