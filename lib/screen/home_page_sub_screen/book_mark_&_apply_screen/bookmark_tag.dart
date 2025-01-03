9020ecf9aacd056509afd7b018c6180296b02a22
YanTing0Volleyball <yanting.wkact810611@gmail.com>
2025-01-03 10:32:20 +0800

    Add Exception Dealing In Bookmark And Apply Page

--- lib/screen/home_page_sub_screen/book_mark_&_apply_screen/bookmark_tag.dart
+++ lib/screen/home_page_sub_screen/book_mark_&_apply_screen/bookmark_tag.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:spark_up/common_widget/empty_view.dart';
 import 'package:spark_up/common_widget/event_card.dart';
+import 'package:spark_up/common_widget/system_message.dart';
 import 'package:spark_up/data/list_receive_post.dart';
 import 'package:spark_up/network/network.dart';
 import 'package:spark_up/network/path/user_path.dart';
@@ -33,7 +34,7 @@ class _BookMarkTagState extends State<BookMarkTag>
     getMoreData();
   }
 
-  void getMoreData() async {
+  Future<void> getMoreData() async {
     if (isLoading || isEnd) return;
 
     isLoading = true;
@@ -53,6 +54,51 @@ class _BookMarkTagState extends State<BookMarkTag>
         pages = response["data"]["pages"];
         isEnd = page >= pages;
         page++;
+      } else if (response["status"] == "error") {
+        switch (response["data"]["message"]) {
+          case "Timeout Error":
+            showDialog(
+                context: context,
+                builder: (context) => const SystemMessage(
+                    title: "Timeout error",
+                    content:
+                        "The response time is too long, please check the connection and try againg later."));
+            break;
+          case "Connection Error":
+            showDialog(
+                context: context,
+                builder: (context) => const SystemMessage(
+                    title: "Connection error",
+                    content:
+                        "The connection is unstable, please check the connection and try again later."));
+            break;
+          default:
+            showDialog(
+                context: context,
+                builder: (context) => const SystemMessage(
+                    title: "Local error",
+                    content:
+                        "An unexpected local error occured, please contact us or try again later."));
+            break;
+        }
+      } else if (response["status"] == "faild") {
+        switch (response["status_code"]) {
+          default:
+            showDialog(
+                context: context,
+                builder: (context) => const SystemMessage(
+                    title: "Server error",
+                    content:
+                        "An unexpected server error occured, please contact us or try againg later."));
+            break;
+        }
+      } else {
+        showDialog(
+            context: context,
+            builder: (context) => const SystemMessage(
+                title: "Error",
+                content:
+                    "An unexpected error occured, please contact us or try again later."));
       }
     }
 
@@ -60,11 +106,11 @@ class _BookMarkTagState extends State<BookMarkTag>
     setState(() {});
   }
 
-  Future refresh() async {
+  Future<void> refresh() async {
     postList.clear();
     page = 1;
     isEnd = false;
-    getMoreData();
+    await getMoreData();
   }
 
   @override
